fe74ffa9ae8476a57a01c1f43d2bb768ef69d58e
Alena Khudnitskaya <elen.g.minsk@gmail.com>
2022-12-02 20:45:54 +0300

    Added SpringBoot skeleton project

---
+++ src/main/java/com/github/AlenaKhudnitskaya/WinterMoodBot/WinterMoodBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.AlenaKhudnitskaya.WinterMoodBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterMoodBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterMoodBotApplication.class, args);
+	}
+
+}
